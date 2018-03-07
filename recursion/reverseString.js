c04438358c62fb13b53c5e2e0acbc3d346760746
Dayan Perera <dayannp@gmail.com>
2018-03-07 12:03:38 -0800

    Recursive reverse String solution-2

--- recursion/reverseString.js
+++ recursion/reverseString.js
@@ -3,6 +3,8 @@ Implement a function that will reverse a string recursively.
 reverse('abcdefg')
 => 'gfedcba'
 */
+
+//--- Solution -1
 function reverseString(str) {
 	//basecase
 	if (str.length === 1) {
@@ -11,5 +13,22 @@ function reverseString(str) {
 	var lastChar = str.substring(str.length - 1);
 	return lastChar.concat(reverseString(str.substring(0, str.length - 1)));
 }
+//--- Solution -2
+function reverseStr(str) {
+	//split the string to character level array
+	var charArray = str.split('');
+	var reversedArry = [];
+	function recursiveReverse(arr) {
+		if (arr.length === 0) {
+			return [];
+		}
+		//concat the last element of the arry to the reversedArray
+		reversedArry = reversedArry.concat(arr.pop());
+		return recursiveReverse(arr);
+	}
 
-console.log(reverseString('abcdefg'));
+	recursiveReverse(charArray);
+	return reversedArry.join('');
+}
+
+console.log(reverseStr('abcdefg'));
